cb8ffd4846c9e5df86997b12e6f547963e74089e
Sasha Ehrenstein <inquisitorsasha@sturmkrieg.ru>
2014-04-03 12:22:09 -0400

    Update

--- listGen936.py
+++ listGen936.py
@@ -2,9 +2,9 @@
 import itertools
 from lib.wordList import words
 
-print words;
-
 for l in range(1,4):
 	combo = itertools.permutations(words, l);
 	for i in combo:
-		print ''.join(i)
+		file = open('phraselist.txt', 'a');
+		file.write(''.join(i)+'\n');
+		file.close();
