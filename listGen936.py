3742b7f855fc984ecd2fe5f76c64316797ed4e18
Sasha <spavelovich@lexipedium.org>
2014-09-08 01:56:46 -0400

    Changing files

--- listGen936.py
+++ listGen936.py
@@ -4,12 +4,22 @@ from lib.wordList import words
 ########################################
 #   Configuration                      #
 ########################################
-maxLen = 4
+#maxLen = 4
 
 ########################################
+
+maxLen = input("What is the maximum number of words that you would like in the string? ")
+printFile = raw_input("Print a wordlist to file? (y/n) ")
+
+if printFile == 'n':
+	raw_input("Enter an XKCD 936 compliant password to match: ")
+
 for l in range(1,maxLen):
-	combo = itertools.permutations(words, l);
+	combo = itertools.permutations(words, l)
 	for i in combo:
-		file = open('phraselist.txt', 'a');
-		file.write(''.join(i)+'\n');
-		file.close();
+		if printFile == 'y':
+			file = open('phraselist.txt', 'a')
+			file.write(''.join(i)+'\n')
+			file.close()
+		else:
+			print "".join(i)+'\n'
